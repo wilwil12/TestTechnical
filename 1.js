f90bb8f94074bd365066806755f641b4948fa3b1
muhammad_wildan <mwildan74@gmail.com>
2020-08-08 21:25:01 +0700

    push nomber 4A

--- 1.js
+++ 1.js
@@ -3,11 +3,14 @@ let KecepatanN = 7
 let Angela = 13.00
 let KecepatanA = 10
 let detik = 3600
+let susul = 60 / (KecepatanA - KecepatanN)
 hitung = () => {
-    let selisih = Angela - Nana
-    let hasil1 = selisih * detik
-    let tujuan = hasil1 / KecepatanN
-    console.log(tujuan)
+    let NanaRange = (detik * KecepatanN) * 3
+    let AngelaRange = (detik * KecepatanA) * 2
+    let jumlahRange = NanaRange + AngelaRange
+    let Papasan = jumlahRange / (KecepatanN + KecepatanA)
+    let jumlah = Papasan / detik
+    let round = Math.round(jumlah)
+    console.log(`Pada pukul ${Angela + round + '.' + susul.toPrecision(2)} Mereka Berada pada titik yang sama `)
 }
-
 hitung()
